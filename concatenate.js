4a394855cf69934b4ad98ea0d4a78fd45a779a6f
reflexdemon <venkateswara.vp@gmail.com>
2021-05-17 16:05:48 -0400

    Code clean-up and added logs

--- concatenate.js
+++ concatenate.js
@@ -8,8 +8,12 @@ const target = 'docs';
     './dist/angular10-webcompponent/runtime.js',
   ]
   await fs.ensureDir(`${target}`)
+  console.log(`Merging all JS files to ${target}/analytics-counter.js`)
   await concat(files, `${target}/analytics-counter.js`);
+  console.log(`Merging all Styles to ${target}/styles.css`)
   await fs.copyFile('./dist/angular10-webcompponent/styles.css', `${target}/styles.css`)
+  console.log(`Merging all Third party licenses to ${target}/3rdpartylicenses.txt`)
   await fs.copy('./dist/angular10-webcompponent/3rdpartylicenses.txt', `${target}/3rdpartylicenses.txt` )
+  console.log(`Copy your favicon.ico if you like :)`)
   await fs.copy('./dist/angular10-webcompponent/favicon.ico', `${target}/favicon.ico` )
 })()
